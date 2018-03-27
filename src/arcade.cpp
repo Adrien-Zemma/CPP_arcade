40b5a7a4a42fed2dd268389923f04a06b0915571
Hugo MARTIN <hugo.martin@epitech.eu>
2018-03-27 12:01:03 +0000

    Nibbler is fixed

--- src/arcade.cpp
+++ src/arcade.cpp
@@ -68,6 +68,7 @@ void	Arcade::loadGame(std::string game)
 	if (game != "")
 	{
 		std::cout << game << std::endl;
+		std::cout << "./games/" + game << std::endl;
 		_handle_game = dlopen(std::string("./games/" + game).data(), RTLD_NOW);
 		std::cout << "dlopen fini"<< std::endl;
 	}
@@ -82,7 +83,8 @@ void	Arcade::loadGame(std::string game)
 	}
 	else
 	{
-		if (_handle_game != NULL)
+		std::cout << "Can't open lib" << std::endl;
+		if (_handle_game == NULL)
 			std::cout << dlerror() << std::endl;
 		_exit_status = true;
 	}
