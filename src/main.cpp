b2981931eb5460441d2d490b736c144096934d97
adrien ZEMMA <adrien.zemma@epitech.eu>
2018-04-01 19:53:18 +0200

    fixing -h

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,25 @@
 
 #include "arcade.hpp"
 
+
+
+
+
+
+
+
 int	main(int ac, char **av)
 {
-	if (ac == 2)
+	if (ac == 2 && 
+	(std::string(av[1]).find("lib_arcade") != std::string::npos &&
+	std::string(av[1]).find(".so") != std::string::npos))
 	{
 		Arcade station(av[1]);
 		station.start();
+	}
+	else
+	{
+		std::cout << "USAGE:\t"<< av[0] << " lib_name" << std::endl;
+		return 84;
 	}
 }
